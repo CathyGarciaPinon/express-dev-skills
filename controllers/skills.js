ddfaea29e664a809a2cb6a1a7a04304e4da5aa42
cathygarciapinon <cathygarciapinon@gmail.com>
2022-10-20 18:40:55 -0700

    Lab part 2 complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,19 +3,37 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill
 };
 
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+  }
+
+function create(req, res) {
+    console.log(req.body);
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new', { title: 'New language'});
+}
+
 function show(req, res) {
-    console.log('skillset', req.params.id)
     res.render('skills/show', {
       skill: Skill.getOne(req.params.id),
+      title: 'Skills Details'
     });
 }
 
 function index(req, res) {
     res.render('skills/index', {
         skills: Skill.getAll(),
-        skillset: 'Languages'
+        title: 'Programming Languages'
     });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -5,6 +5,12 @@ const skillsCtrl = require('../controllers/skills');
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
 
-router.get('/:id', skillsCtrl.show)
+router.get('/new', skillsCtrl.new);
+
+router.get('/:id', skillsCtrl.show);
+
+router.post('/', skillsCtrl.create);
+
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
